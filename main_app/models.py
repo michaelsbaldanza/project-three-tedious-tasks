2785157fed4020559d65f8f294962c0dfb3186e4
Michael Baldanza <michael.snider.baldanza@gmail.com>
2020-09-11 16:01:29 -0400

    add css and model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    description = models.CharField(max_length=100)
+    time = models.IntegerField
+    person = models.CharField(max_length=100)
