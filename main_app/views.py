9c4379cf8e2a195eacefadbb118e31be9ddfaa26
Michael Baldanza <michael.snider.baldanza@gmail.com>
2020-09-11 16:57:46 -0400

    add form and empty table

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Task
+from .forms import TaskForm
 from django.http import HttpResponse
 
 def index(request):
     tasks_list = Task.objects.all()
-    return render(request, 'index.html', { 'tasks_list': tasks_list})
+    task_form = TaskForm()
+    return render(request, 'index.html', {
+        'tasks_list': tasks_list,
+        'task_form': task_form
+        })
